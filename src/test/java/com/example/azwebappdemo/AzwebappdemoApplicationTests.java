6c6e6620398f0071f762cda86a9d0c6d2f1c46b7
chandrasamanta <samanta.chandrakanta@gmail.com>
2021-10-24 10:43:05 +0530

    first commit

---
+++ src/test/java/com/example/azwebappdemo/AzwebappdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azwebappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzwebappdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
